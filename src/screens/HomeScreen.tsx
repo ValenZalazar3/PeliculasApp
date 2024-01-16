0cee4d22621ddc7a731917315ef5f51c864daa93
Valen <valenzalazar3@gmail.com>
2024-01-16 09:18:59 -0300

    Peliculas

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
 import Carousel from 'react-native-snap-carousel';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {ActivityIndicator, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import {useMovies} from '../hooks/useMovies';
 import {MoviePoster} from '../component/MoviePoster';
-import {Movie} from '../interfaces/movieInterface';
+import {HorizontalSlider} from '../component/HorizontalSlider';
+
+const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
   const {peliculasEnCine, isLoading} = useMovies();
   const {top} = useSafeAreaInsets();
 
-  if (isLoading || undefined) {
+  if (isLoading) {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <ActivityIndicator color="red" size={100} />
       </View>
     );
   }
-  console.log('Valentin', peliculasEnCine);
   return (
-    <View style={{marginTop: top + 20}}>
-      <MoviePoster movie={peliculasEnCine[0]} />
-      <Carousel
-        data={peliculasEnCine}
-        sliderWidth={350}
-        itemWidth={300}
-        renderItem={item => <MoviePoster movie={item.item} />}
-      />
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20}}>
+        {/* Carrusel principal */}
+        <View style={{height: 440}}>
+          <Carousel
+            data={peliculasEnCine}
+            sliderWidth={windowWidth}
+            itemWidth={300}
+            renderItem={({item}) => <MoviePoster movie={item} />}
+            inactiveSlideOpacity={0.9}
+          />
+        </View>
+
+        {/*  Peliculas Populares */}
+
+        <HorizontalSlider title="En cine" movies={peliculasEnCine} />
+      </View>
+    </ScrollView>
   );
 };
